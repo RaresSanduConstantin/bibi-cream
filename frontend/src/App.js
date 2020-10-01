74d7df9304c5b292580c12caf0f182131fc9bca3
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-10-01 21:11:53 +0300

    header, footer and main area

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <div>
-      <h1>Welcome to Bibi's cream</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 };
