16e05c1954c8460f443ba3667c82bf3d98f63684
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-10-03 11:31:32 +0300

    backend

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,7 +1,7 @@
 const products = [
   {
     _id: "1",
-    name: "Cream Coconut",
+    name: "Bibi's Cream Coconut",
     image: "/cream/coconut.jpg",
     description:
       "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
@@ -14,46 +14,46 @@ const products = [
   },
   {
     _id: "2",
-    name: "Best skin cleaner",
+    name: "Bibi's skin cleaner",
     image: "/cream/curology.jpg",
     description:
       "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
     brand: "Apple",
     category: "Electronics",
-    price: 599.99,
+    price: 59.99,
     countInStock: 7,
     rating: 4.0,
     numReviews: 8,
   },
   {
     _id: "3",
-    name: "Essents with nice smell",
+    name: "Bibi's Vanilla Essents",
     image: "/cream/essents.jpg",
     description:
       "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
     brand: "Cannon",
     category: "Electronics",
-    price: 929.99,
+    price: 92.99,
     countInStock: 5,
     rating: 3,
     numReviews: 12,
   },
   {
     _id: "4",
-    name: "Galbenele Cream",
+    name: "Bibi's Galbenele Cream",
     image: "/cream/galbenele.jpg",
     description:
       "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
     brand: "Sony",
     category: "Electronics",
-    price: 399.99,
+    price: 39.99,
     countInStock: 11,
     rating: 5,
     numReviews: 12,
   },
   {
     _id: "5",
-    name: "Best No-Name Cream",
+    name: "Bibi's Hands Cream",
     image: "/cream/no-name.jpg",
     description:
       "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
@@ -66,7 +66,7 @@ const products = [
   },
   {
     _id: "6",
-    name: "Skin cream",
+    name: "Bibi's Skin Cream",
     image: "/cream/skin-cream.jpg",
     description:
       "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
